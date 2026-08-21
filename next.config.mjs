72610350c53207e3121e817e61b0bdac460d406d
Claire Garcia <garciaclaire520@gmail.com>
2026-08-21 14:21:23 +0800

    fix github pages base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
   basePath: "/Pangasinan-heritage-showcase",
-  assetPrefix: "/Pangasinan-heritage-showcase/",
-
   images: {
     unoptimized: true,
   },
